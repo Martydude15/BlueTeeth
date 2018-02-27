005e510aec37a1dc77a828efd3833bba0fd6b14f
Martydude15 <mdh1525@jagmail.southalabama.edu>
2018-02-27 13:05:42 -0600

    2nd Activity Added

--- app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
+++ app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.example.marta.blueteeth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+
+    public void testMethod(View view) {
+        Intent switchPage = new Intent(HomeScreen.this, StudentHomeActivity.class);
+        startActivity(switchPage);
     }
 }
