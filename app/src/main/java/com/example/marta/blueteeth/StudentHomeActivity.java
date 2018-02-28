66821da03d52dbc151c4ad1dfe3ff04da4f8a568
Martydude15 <mdh1525@jagmail.southalabama.edu>
2018-02-27 23:52:11 -0600

    More Screens

--- app/src/main/java/com/example/marta/blueteeth/StudentHomeActivity.java
+++ app/src/main/java/com/example/marta/blueteeth/StudentHomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.marta.blueteeth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentHomeActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class StudentHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
     }
+
+    public void testMethod(View view) {
+        Intent switchPage = new Intent(StudentHomeActivity.this, StudentConnectScreen.class);
+        startActivity(switchPage);
+    }
+
 }
