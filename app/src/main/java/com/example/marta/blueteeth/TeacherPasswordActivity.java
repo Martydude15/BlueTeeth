0df85f8ec1e8a964aeee32cb7c83f0636847dace
randyjr <randydsmithhr@gmail.com>
2018-02-27 20:02:25 -0600

    Working correctly on mine now

---
+++ app/src/main/java/com/example/marta/blueteeth/TeacherPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.marta.blueteeth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_password);
+    }
+}
