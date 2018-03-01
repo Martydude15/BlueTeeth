c43e1ee31b75b5b536bfa0b59faf15f42bdef7a3
randyjr <randydsmithhr@gmail.com>
2018-03-01 13:21:38 -0600

    Added JSON reader as well as JSON file. Check app\main\assets\

--- app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
+++ app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
@@ -1,13 +1,10 @@
 package com.example.marta.blueteeth;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
