ee18f41b76f6dc438218f9f9b2c50d148f55298e
dmelton13 <dontarriom@yahoo.com>
2018-03-10 22:21:29 -0600

    More screens, looking at the app draw

--- app/src/main/java/com/example/marta/blueteeth/TeacherPhoto.java
+++ app/src/main/java/com/example/marta/blueteeth/TeacherPhoto.java
@@ -1,8 +1,30 @@
 package com.example.marta.blueteeth;
 
-/**
- * Created by donta on 3/7/2018.
- */
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.io.File;
+import java.io.IOException;
 
-public class TeacherPhoto {
+public class TeacherPhoto extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_photo);
+    }
+
+        public void testMethod(View view) {
+            Intent switchPage = new Intent(TeacherPhoto.this, TeacherHomeScreenActivity.class);
+            startActivity(switchPage);
+        }
+
 }
