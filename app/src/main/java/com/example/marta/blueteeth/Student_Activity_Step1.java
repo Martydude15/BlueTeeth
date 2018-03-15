a180de432b295249ae3f7efc12a048c4359f44e7
Shisha <baj1621@jagmail.southalabama.edu>
2018-03-15 15:27:35 -0500

    the methnods for the camera are not being picked up by the XML i believe. Left the xml file the original way so can be altered

--- app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
+++ app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
@@ -2,6 +2,7 @@ package com.example.marta.blueteeth;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -13,71 +14,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Student_Activity_Step1 extends AppCompatActivity {
-/*
-    int TAKE_PHOTO_CODE = 0;
-    public static int count = 0;
-*/
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_step1);
-
-        /*
-        // Here, we are making a folder named picFolder to store
-        // pics taken by the camera using this application.
-        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
-        File newdir = new File(dir);
-        newdir.mkdirs();
-
-        Button capture = (Button) findViewById(R.id.btnCamera);
-        capture.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                // Here, the counter will be incremented each time, and the
-                // picture taken by camera will be stored as 1.jpg,2.jpg
-                // and likewise.
-                count++;
-                String file = dir+count+".jpg";
-                File newfile = new File(file);
-                try {
-                    newfile.createNewFile();
-                }
-                catch (IOException e)
-                {
-                }
-
-                Uri outputFileUri = Uri.fromFile(newfile);
-
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
-            }
-        });
-
-
-    }
+    ImageView imageView;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_student_step1);
 
-        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
-            Log.d("CameraDemo", "Pic saved");
+            Button btncamera = (Button)findViewById(R.id.btnCamera);
+            ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+            btncamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent,0);
+                }
+            });
         }
-    }
-    */
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
     }
 
-    public void testMethod(View view) {
-        Intent switchPage = new Intent(Student_Activity_Step1.this, Student_Step_2.class);
-        startActivity(switchPage);
-    }
-
-}
