ee3b6e32ab7d0006e29b349361d2b5b3bc9a78c8
randyjr <randydsmithhr@gmail.com>
2018-03-16 23:19:41 -0500

    Added comments and fixed up the json file so it follows class constructors more concisely

--- app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
+++ app/src/main/java/com/example/marta/blueteeth/HomeScreen.java
@@ -18,41 +18,75 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
     }
 
-
-    public void login(View view) throws IOException {
+    /**
+     *
+     * @param view
+     *      Takes in the current "view" which is just the current screen that is built
+     *      as defined in activity_home_screen.xml in the layouts folder
+     *      to modify via a button click.
+     */
+    public void login(View view) {
         try {
+            // Pulls the textBox defined in the layout
             EditText textBox = findViewById(R.id.editText);
+            // Sets the Intent returned from verifyLogin
             Intent intent = verifyLogin(textBox.getText().toString());
+            // If the intent is not null, start the activity
             if (intent != null) startActivity(intent);
+            // If it was null, it means the JagNumber was not found
             else {
+                // Clears to allow for re-entry
                 textBox.getText().clear();
                 AlertDialog dialog = loginError();
                 dialog.show();
             }
         }
         catch (IOException ioe) {
+            // Just here to handle the case of the file not being found.
             AlertDialog dialog = loginError();
+            // Changes the message to say File not found.
             dialog.setMessage("File not found.");
             dialog.show();
         }
     }
 
+    /**
+     *
+     * @param jagNumber
+     *      taking in the text from the text field called editText
+     * @return
+     *      returns the intent depending on if the jagNumber matches a teacher or a student
+     *      otherwise return null to catch later
+     * @throws IOException
+     */
     public Intent verifyLogin(String jagNumber) throws IOException {
+        // Create new JSONDriver to handle the json file.
         JSONDriver json = new JSONDriver(getAssets().open("login.json"));
+        // Processes the json file and puts into corresponding lists.
         json.getPeople(json.getJson());
+        // Checks if the jagNumber returned from the textBox is a teacher
         for (Teacher teacher : json.getTeachers()) {
             if (teacher.getJagNumber().equals(jagNumber)) {
+                // If teacher, go to next part for teacher
                 return new Intent(HomeScreen.this, TeacherHomeActivity.class);
             }
         }
+        // Checks if the jagNumber returned from the textBox is a student
         for (Student student : json.getStudents()) {
              if (student.getJagNumber().equals(jagNumber)) {
+                 // If student, go to next part for student
                  return new Intent(HomeScreen.this, StudentHomeActivity.class);
              }
         }
         return null;
     }
 
+    /**
+     *
+     * @return
+     *      returns an alert dialog box to handle errors for File not found, and JagNumber
+     *      not found.
+     */
     public AlertDialog loginError() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Jag Number does not exist.");
--- app/src/main/java/com/example/marta/domain/JSONDriver.java
+++ app/src/main/java/com/example/marta/domain/JSONDriver.java
@@ -13,25 +13,61 @@ public class JSONDriver {
     private List<Teacher> teachers;
     private List<Student> students;
 
+    /**
+     *
+     * @param pathToJson
+     *      take the path to the json file that contains the information of the class
+     * @throws IOException
+     *      handled in HomeScreen.java
+     */
     public JSONDriver(InputStream pathToJson) throws IOException {
+        /*
+        Pulls in the path as InputStream because it is a super class of InputStreamReader
+        and allows for a string to be pass as the filename.
+         */
          json = new JsonReader(new InputStreamReader(pathToJson, "UTF-8"));
+         // Makes sure for each new instance of JSONDriver it has a fresh list
          teachers = new ArrayList<>();
          students = new ArrayList<>();
     }
 
+    /**
+     *
+     * @param json
+     *      receives the open Json file to be read
+     * @throws IOException
+     *      handled in HomeScreen.java
+     */
     public void getPeople(JsonReader json) throws IOException {
+        /*
+        Points to everything in json file that is surrounded by []
+         */
         json.beginArray();
+        // While it still has things encapsulated by []
         while (json.hasNext()) {
+            // Just to keep the method shorter, passed work off to readPerson
             readPerson(json);
         }
+        // After it has nothing left, close the file reader
         json.endArray();
     }
 
+    /**
+     *
+     * @param json
+     *      receives the open Json file to be read
+     * @throws IOException
+     *      handled in HomeScreen.java
+     */
     private void readPerson(JsonReader json) throws IOException {
+        // Looks at everything encapsulated by {}
         json.beginObject();
         String type = "", fname = "", mname = "", lname = "", jagnumber = "", password = "";
+        // While there is anything left to read in {}
         while (json.hasNext()) {
+            // Pulls the next key or string defined on left side
             String key = json.nextName();
+            // Lowers the casing for better checking
             switch (key.toLowerCase()) {
                 case "class":
                     type = json.nextString();
@@ -52,13 +88,16 @@ public class JSONDriver {
                     password = json.nextString();
                     break;
                 default:
+                    // Just here in case
                     json.skipValue();
                     break;
             }
+            // If teacher, add to teachers
             if (type.toLowerCase().equals("teacher")) {
                 this.teachers
                         .add(new Teacher(fname, mname, lname, jagnumber, password));
             }
+            // If student, add to students
             else if (type.toLowerCase().equals("student")) {
                 this.students
                         .add(new Student(fname, mname, lname, jagnumber));
@@ -67,15 +106,29 @@ public class JSONDriver {
         json.endObject();
     }
 
-
+    /**
+     *
+     * @return
+     *      returns the json file so it can be handled in different classes
+     */
     public JsonReader getJson() {
         return this.json;
     }
 
+    /**
+     *
+     * @return
+     *      returns the list of teachers found in the Json file
+     */
     public List<Teacher> getTeachers() {
         return teachers;
     }
 
+    /**
+     *
+     * @return
+     *      returns the list of students found in the Json file
+     */
     public List<Student> getStudents() {
         return students;
     }
