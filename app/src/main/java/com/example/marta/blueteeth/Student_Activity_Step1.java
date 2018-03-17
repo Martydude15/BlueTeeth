2e74c66480732463ccce8218a0bf777533dd1534
randyjr <randydsmithhr@gmail.com>
2018-03-17 00:42:57 -0500

    Added request permission in Student_Activity_Step1 to take picture.

--- app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
+++ app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
@@ -1,7 +1,11 @@
 package com.example.marta.blueteeth;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +16,13 @@ public class Student_Activity_Step1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_step1);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Ask for it
+            ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
     }
 
     public void cameraClick(View view) {
