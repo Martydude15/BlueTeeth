16871e445b54a731287ce90ed27fad910b047b22
randyjr <randydsmithhr@gmail.com>
2018-03-17 16:06:07 -0500

    Reimplemented Blake's method to capture the image. Loses if you change the orientation.

--- app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
+++ app/src/main/java/com/example/marta/blueteeth/Student_Activity_Step1.java
@@ -3,12 +3,14 @@ package com.example.marta.blueteeth;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Student_Activity_Step1 extends AppCompatActivity {
 
@@ -28,5 +30,13 @@ public class Student_Activity_Step1 extends AppCompatActivity {
     public void cameraClick(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageBitmap(bitmap);
     }
 }
